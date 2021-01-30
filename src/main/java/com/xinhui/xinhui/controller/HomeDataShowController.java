03deb2322979f587e329cd1ca3db4a4c3ed51eca
wangdongliang <wangdongliang@17zhiliao.com>
2021-01-30 18:10:19 +0800

    添加产品材料

--- src/main/java/com/xinhui/xinhui/controller/HomeDataShowController.java
+++ src/main/java/com/xinhui/xinhui/controller/HomeDataShowController.java
@@ -1,7 +1,6 @@
 package com.xinhui.xinhui.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
